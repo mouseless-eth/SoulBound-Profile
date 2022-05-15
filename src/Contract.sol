2f4bb68bc8d759dad13de449927ad6067d5003d7
NME.eth <maximus.holm@gmail.com>
2022-05-15 20:39:00 +0300

    initializing repo

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Contract {}
+import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+
+abstract contract SoulBound is ERC721 {
+  // @notice Error is thrown when trying to transfer a Soulbound token
+  error SoulBound();
+
+  
+}
